9c9b10131b7050e61e3bea5c99015c92dbec3eb6
Blunacy <99257101+Blunacy@users.noreply.github.com>
2023-05-27 21:38:11 +0900

    JPA 쿼리 메소드

--- src/main/java/com/lunacy/shop/repository/ItemRepository.java
+++ src/main/java/com/lunacy/shop/repository/ItemRepository.java
@@ -3,5 +3,9 @@ package com.lunacy.shop.repository;
 import com.lunacy.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+  List<Item> findByItemNm(String itemNm);
 }
